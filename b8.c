08b4247a1ff637a536a20dfc8978e54cb3350117
ducle070106 <ducle070106@gmail.com>
2025-08-20 10:35:31 +0700

    Add C pointer exercises

---
+++ b8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char inputString[100];
+    char reverseString[100];
+    printf("Nhap vao mot chuoi: ");
+    scanf("%s", inputString);
+    return 0;
+}
